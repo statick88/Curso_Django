9c690eb2dcf0b666a6fd80f6cb49fe1accb002a1
Diego Saavedra <dsaavedra88@gmail.com>
2023-07-21 22:35:40 -0500

    Codigo Modulo 2

--- Codigo/blog/settings.py
+++ Codigo/blog/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# settings.py
+
+# settings.py
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'D3mon1o.',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
